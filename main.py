f1a6a0c35c4a297821b37dacc3dca327e3389e0c
Rutesh Patel <patelrutesh143@gmail.com>
2025-07-11 16:51:27 +0530

    Modify a router path.

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(kyc_routers)
+app.include_router(kyc_routers, prefix="/api/kyc")
 
 if __name__ == "__main__":
     uvicorn.run(
